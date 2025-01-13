e7f3923f4b989c45a95e539e5e27254575bface7
Dmyan <dmyan@msn.com>
2025-01-13 22:26:03 +0000

    fixes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,27 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from "path"; 
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // 
   // without `base` works perfectally for `npx`
   //
-  base: "/wedding-guest-grove-60/",
+  base: mode === "production" ? "/wedding-guest-grove-60/" : "/", 
   build: {
-    // outDir: "/app/dist",
+    // Define o diretório de saída do build
+    outDir: "dist", 
+
+    // Aviso para arquivos grandes no build (ajuste conforme necessário)
     chunkSizeWarningLimit: 1600,
+
+    // Geração de source maps para facilitar a depuração
+    sourcemap: mode === "production" ? false : true,
   },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
+    // Plugin para suporte ao React
     react(),
-    // mode === 'development' && componentTagger(),
-    componentTagger()
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
