bc5143931e4c47847186fd75ed1c5ba675a464a1
Dmyan <dmyan@msn.com>
2025-01-13 15:27:53 +0000

    fixes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,8 +9,10 @@ export default defineConfig(({ mode }) => ({
   // // base: mode === "production" ? "/wedding-guest-grove-60/app/" : "/", // Base path for production
   // base: "/wedding-guest-grove-60/app/",
 
-  base: mode === "production" ? "/wedding-guest-grove-60/app/" : "/", // Base path for production
+  // 
   // without `base` works perfectally for `npx`
+  //
+  
   build: {
     // outDir: "/app/dist",
     chunkSizeWarningLimit: 1600,
